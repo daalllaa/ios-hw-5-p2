4ea2ce485309052eb558981689116d8f9a94d298
daallaa <dalal.alj003@GMAIL.COM>
2020-07-09 23:57:31 +0300

    I have a problem with assistant

---
+++ TableView5/TableView5/TableViewCell.swift
@@ -0,0 +1,24 @@
+///Users/dalalaljouhar/Documents/ios-hw-5-p2/TableView5/TableView5/TableViewCell.swift
+//  TableViewCell.swift
+//  TableView5
+//
+//  Created by Dalal Aljouhar on 7/9/20.
+//  Copyright © 2020 Dalal Aljouhar. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
